d5cea7671009d14a37ebf9a308f16c2ca0087c92
YaadLuria <yaad.luria@mail.huji.ac.il>
2022-04-26 23:19:18 +0300

    ex3

--- IMLearn/learners/classifiers/perceptron.py
+++ IMLearn/learners/classifiers/perceptron.py
@@ -12,26 +12,19 @@ def default_callback(fit: Perceptron, x: np.ndarray, y: int):
 class Perceptron(BaseEstimator):
     """
     Perceptron half-space classifier
-
     Finds a separating hyperplane for given linearly separable data.
-
     Attributes
     ----------
     include_intercept: bool, default = True
         Should fitted model include an intercept or not
-
     max_iter_: int, default = 1000
         Maximum number of passes over training data
-
     coefs_: ndarray of shape (n_features,) or (n_features+1,)
         Coefficients vector fitted by Perceptron algorithm. To be set in
         `Perceptron.fit` function.
-
-    training_loss_: array of floats
-        holds the loss value of the algorithm during training.
-        training_loss_[i] is the loss value of the i'th training iteration.
-        to be filled in `Perceptron.fit` function.
-
+    callback_: Callable[[Perceptron, np.ndarray, int], None]
+            A callable to be called after each update of the model while fitting to given data
+            Callable function should receive as input a Perceptron instance, current sample and current response
     """
     def __init__(self,
                  include_intercept: bool = True,
@@ -39,33 +32,15 @@ class Perceptron(BaseEstimator):
                  callback: Callable[[Perceptron, np.ndarray, int], None] = default_callback):
         """
         Instantiate a Perceptron classifier
-
         Parameters
         ----------
         include_intercept: bool, default=True
             Should fitted model include an intercept or not
-
         max_iter: int, default = 1000
             Maximum number of passes over training data
-
         callback: Callable[[Perceptron, np.ndarray, int], None]
             A callable to be called after each update of the model while fitting to given data
             Callable function should receive as input a Perceptron instance, current sample and current response
-
-        Attributes
-        ----------
-        include_intercept_: bool
-            Should fitted model include an intercept or not
-
-        max_iter): int, default = 1000
-            Maximum number of passes over training data
-
-        callback_: Callable[[Perceptron, np.ndarray, int], None]
-            A callable to be called after each update of the model while fitting to given data
-            Callable function should receive as input a Perceptron instance, current sample and current response
-
-        coefs_: ndarray of shape (n_features,) or (n_features+1,)
-            Coefficients vector fitted by Perceptron. To be set in `Perceptron.fit` function.
         """
         super().__init__()
         self.include_intercept_ = include_intercept
@@ -77,15 +52,12 @@ class Perceptron(BaseEstimator):
         """
         Fit a halfspace to to given samples. Iterate over given data as long as there exists a sample misclassified
         or that did not reach `self.max_iter_`
-
         Parameters
         ----------
         X : ndarray of shape (n_samples, n_features)
             Input data to fit an estimator for
-
         y : ndarray of shape (n_samples, )
             Responses of input data to fit to
-
         Notes
         -----
         Fits model with or without an intercept depending on value of `self.fit_intercept_`
@@ -95,12 +67,10 @@ class Perceptron(BaseEstimator):
     def _predict(self, X: np.ndarray) -> np.ndarray:
         """
         Predict responses for given samples using fitted estimator
-
         Parameters
         ----------
         X : ndarray of shape (n_samples, n_features)
             Input data to predict responses for
-
         Returns
         -------
         responses : ndarray of shape (n_samples, )
@@ -111,18 +81,16 @@ class Perceptron(BaseEstimator):
     def _loss(self, X: np.ndarray, y: np.ndarray) -> float:
         """
         Evaluate performance under misclassification loss function
-
         Parameters
         ----------
         X : ndarray of shape (n_samples, n_features)
             Test samples
-
         y : ndarray of shape (n_samples, )
             True labels of test samples
-
         Returns
         -------
         loss : float
             Performance under missclassification loss function
         """
+        from ...metrics import misclassification_error
         raise NotImplementedError()
